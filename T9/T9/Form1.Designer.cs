b947e981178c3abf802815fe4aec2faebf36e26e
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-03-31 18:37:57 +0600

    Add files via upload

---
+++ T9/T9/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace T9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 198);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.IsSpace);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("PMingLiU-ExtB", 15.45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(190, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("PMingLiU-ExtB", 15.45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Чтобы слово предложенное слово";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "добавилось нажмите SPACE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "выделите его и нажмите Add";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Чтобы добавить слово в словарь";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(294, 415);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(310, 488);
+            this.MinimumSize = new System.Drawing.Size(310, 355);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ T9/T9/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T9
+{
+    
+    public partial class Form1 : Form
+    {
+        private List<string> dictionary = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool isTyped = false;
+        private delegate void Change(string text);
+        private delegate void ChangeSelect(int startIndex, int length);
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.SelectedText.Trim() == "") return;
+            dictionary.Add(textBox1.SelectedText);
+        }
+
+        private void CheckWords()
+        {
+            foreach(var word in dictionary)
+            {
+                var splited = textBox1.Text.Split(' ',',','.',':','!','?','(',')','*','-','/','[',']','&','^','$','#');
+                var inputText = splited[splited.Length - 1];
+                if (inputText == "") return;                
+                if (word.Substring(0, inputText.Length) == inputText)
+                {
+                    if (word.Length == inputText.Length) return;
+                    string stringToEnd = word.Substring(inputText.Length, word.Length - inputText.Length);
+                    int index = word.Length -inputText.Length ;
+
+                    textBox1.Invoke(new Change((x) => { textBox1.Text += x; }), stringToEnd);
+                    textBox1.Invoke(new ChangeSelect((x,y) => { textBox1.SelectionStart = x; textBox1.SelectionLength = y; }),textBox1.Text.Length-index,index);
+                    break;
+                }
+            }
+        }
+
+
+
+        private void KeyPress(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyPress(object sender, KeyEventArgs e)
+        {
+            if (textBox1.SelectionLength > 0)
+            {
+                textBox1.Text.Remove(textBox1.SelectionStart, textBox1.SelectionLength);
+            }
+            Thread thread = new Thread(CheckWords)
+            {
+                IsBackground = true
+            };
+            thread.Start();            
+        }
+
+        private void IsSpace(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && textBox1.SelectionLength > 0)
+            {
+
+                textBox1.SelectionLength = 0;
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.SelectionLength = 0;
+
+            }
+        }
+    }
+}
